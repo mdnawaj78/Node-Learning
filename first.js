7c4789c5778aab462731e9936aa55b3f1089c567
mdnawaj78 <83968771+mdnawaj78@users.noreply.github.com>
2024-10-15 20:10:13 +0530

    Node Learning

---
+++ first.js
@@ -0,0 +1,8 @@
+console.log("First java script");
+console.log("Hello java script you are amazing");
+
+let a = 5;
+const b = 6;
+var c = 7;
+console.log(a+b+c)
+console.log("server running on port 5000")
